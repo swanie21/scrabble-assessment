d485ef89163252de34f40bc1f2e9bc22efe0ea5b
swanie21 <kswanie21@gmail.com>
2016-09-19 17:51:02 -0600

    pseudo code tests

---
+++ test/index.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+
+describe('test bundle', function () {
+  it('should work', function () {
+    assert(true);
+    });
+  });
+
+describe('scoreWord function', function () {
+    it.skip('should be a function', function () {
+      assert.isFunction(scoreWord());
+    });
+
+    it.skip('should take a word and return the total score of the word', function () {
+
+    });
+
+    it.skip('should return 8 with the word hello', function () {
+
+    });
+
+    it.skip('should return 0 if it is an empty string or non-string', function () {
+
+    });
+
+    it.skip('should exclude whitespace bafore and after the word', function () {
+
+    });
+
+    it.skip('should take an optional second argument for the word nultiplier', function () {
+
+    });
+
+    it.skip('should have an input field', function () {
+
+    });
+});
